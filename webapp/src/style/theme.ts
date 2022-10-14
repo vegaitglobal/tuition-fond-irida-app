72bfaaca6be7f206d0067449cb7bf25bb3b3d52b
Ana Svitlica <a.svitlica@vegait.rs>
2022-10-14 21:53:31 +0200

    add breakpoints

--- webapp/src/style/theme.ts
+++ webapp/src/style/theme.ts
@@ -1,9 +1,12 @@
 import { Colors, colors } from "./colors";
+import { DeviceSize, breakpoints } from "./breakpoints";
 
 interface Theme {
     colors: Colors;
+    breakpoints: DeviceSize;
 }
 
 export const theme: Theme = {
     colors,
+    breakpoints,
 };
