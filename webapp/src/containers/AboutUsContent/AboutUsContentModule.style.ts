48f37bcfb7288dff306d436740f964168616a988
Rade Tontic <r.tontic@vegait.rs>
2022-10-15 22:26:30 +0200

    Add content

--- webapp/src/containers/AboutUsContent/AboutUsContentModule.style.ts
+++ webapp/src/containers/AboutUsContent/AboutUsContentModule.style.ts
@@ -1,4 +1,47 @@
 import styled from "styled-components";
 
 export const StyledAboutUsContentModule = styled.div`
-`
+    position: relative;
+    padding: 90px 0 210px 178px;
+
+    .first-section {
+        display: flex;
+        justify-content: center;
+    }
+
+    .second-section {
+        display: flex;
+        justify-content: center;
+    }
+
+    .content {
+        min-width: 772px;
+    }
+
+    & img {
+        margin-left: 100px;
+        margin-right: 178px;
+    }
+
+    & h1 {
+        color: #5F4477;
+        font-weight: 600;
+        font-size: 48px;
+        margin-bottom: 60px;
+        line-height: 110%;
+    }
+
+    & p {
+        color: #5F4477;
+        font-weight: 400;
+        font-size: 16px;
+        max-width: 80%;
+        margin-bottom: 90px;
+        line-height: 17.6px;
+    }
+
+    & .button-wrap {
+        display: flex;
+        gap: 20px;
+    }
+`;
--- webapp/src/containers/AboutUsContent/AboutUsContentModule.tsx
+++ webapp/src/containers/AboutUsContent/AboutUsContentModule.tsx
@@ -3,45 +3,40 @@ import { StyledAboutUsContentModule } from "./AboutUsContentModule.style";
 interface Props {
     title1: string;
     description1: string;
-    image1: string;
-    image1Left: boolean;
     title2: string;
     description2: string;
-    image2: string;
-    image2Left: boolean;
+    image: string;
+    imageLeft: boolean;
 }
 
 export const AboutUsContentModule = (props: Props) => {
-    const { title1, description1, image1, image1Left, title2, description2, image2, image2Left } = props;
+    const { title1, description1, image, imageLeft, title2, description2 } = props;
 
     return (
         <StyledAboutUsContentModule>
-            {image1Left ? (
-                <>
-                <img src={image1} alt={title1} />
-                <h1>{title1}</h1>
-                <p>{description1}</p>
-                </>
-            ) : (
-                <>
-                <h1>{title1}</h1>
-                <p>{description1}</p>
-                <img src={image1} alt={title1} />
-                </>
-            )}
-            {image2Left ? (
-                <>
-                <img src={image2} alt={title2} />
-                <h1>{title2}</h1>
-                <p>{description2}</p>
-                </>
-            ) : (
-                <>
-                <h1>{title2}</h1>
-                <p>{description2}</p>
-                <img src={image2} alt={title2} />
-                </>
-            )}
+            <div className="first-section">
+                {imageLeft ? (
+                    <>
+                    <img src={image} alt={title1} />
+                    <div className="content">
+                        <h1>{title1}</h1>
+                        <p>{description1}</p>
+                        <h1>{title2}</h1>
+                        <p>{description2}</p>
+                    </div>
+                    </>
+                ) : (
+                    <>
+                    <div className="content">
+                        <h1>{title1}</h1>
+                        <p>{description1}</p>
+                        <h1>{title2}</h1>
+                        <p>{description2}</p>
+                    </div>
+                    <img src={image} alt={title1} />
+                    </>
+                )}
+            </div>
         </StyledAboutUsContentModule>
     )
 }
