fabf3898f6e19f948a2906637563bf28b33fc21d
Igor Malesevic <igormalesevic98@gmail.com>
2022-10-15 17:56:18 +0200

    wiš

--- webapp/src/core/services/product.service.ts
+++ webapp/src/core/services/product.service.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { Product } from "../models/product";
 
 export const fetchProducts = async (): Promise<Product[]> => {
-    const response = await axios.get<Product[]>("https://localhost:7042/api/Product");
+    const baseUrl = process.env.REACT_APP_API_URL;
+    const response = await axios.get<Product[]>(`${baseUrl}/api/Product`);
     return response.data;
 };
--- webapp/src/pages/DonatePage/DonatePage.tsx
+++ webapp/src/pages/DonatePage/DonatePage.tsx
@@ -3,9 +3,10 @@ import { Product } from "../../core/models/product";
 import { fetchProducts } from "../../core/services";
 
 export const DonatePage = () => {
+    console.log("render");
     const [products, setProducts] = useState<Product[]>([]);
     useEffect(() => {
-        fetchProducts().then((products) => setProducts(products));
+        fetchProducts().then((products: Product[]) => setProducts(products));
     }, []);
 
     return <>
