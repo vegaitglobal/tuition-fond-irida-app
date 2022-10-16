a7f4459dbcdc413ea54d93a73eb6a751af63b150
Zorana Babic <z.babic@vegait.rs>
2022-10-16 16:28:09 +0200

    Handle empty picture

--- TuitionFondIrida/TuitionFondIrida.Persistence/Mappers/AssetMapper.cs
+++ TuitionFondIrida/TuitionFondIrida.Persistence/Mappers/AssetMapper.cs
@@ -1,3 +1,4 @@
+
 using TuitionFondIrida.Domain.Models.Read;
 using TuitionFondIrida.Persistence.Mappers.Abstractions;
 
@@ -5,10 +6,19 @@ namespace TuitionFondIrida.Persistence.Mappers;
 
 public class AssetMapper : IAssetMapper
 {
+    private Contentful.Core.Models.File defaultFileImage;
+
+    public AssetMapper(){
+        defaultFileImage = new Contentful.Core.Models.File();
+    }
     public Asset Create(Contentful.Core.Models.Asset asset)
-    {
-        var file = asset.File;
-        var domainFile = new Domain.Models.Read.File(file.FileName, file.ContentType, file.Url);
-        return new Asset(asset.Description, asset.Title, domainFile);
+    {        
+        if( asset != null){
+            var file =  asset.File;
+            var domainFile = new Domain.Models.Read.File(file.FileName, file.ContentType, file.Url);
+            return new Asset(asset.Description, asset.Title, domainFile);
+        }
+
+        return new Asset("", "", new Domain.Models.Read.File("","",""));
     }
 }
