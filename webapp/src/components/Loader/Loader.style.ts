de6abe0a6be64b4fcb8e2852b07aff523b15ac02
Ana Svitlica <a.svitlica@vegait.rs>
2023-01-10 15:52:09 +0100

    Create component

---
+++ webapp/src/components/Loader/Loader.style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledLoader = styled.div`
+  width: 48px;
+  height: 48px;
+  border: 5px solid ${(props) => props.theme.colors.primary.main};
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
---
+++ webapp/src/components/Loader/Loader.tsx
@@ -0,0 +1,5 @@
+import { StyledLoader } from "./Loader.style";
+
+export const Loader = () => {
+    return <StyledLoader></StyledLoader>;
+};
