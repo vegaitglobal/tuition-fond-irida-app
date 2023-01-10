a4bd8a4acb4ac744bd8c0d46c23f2458f7d1e939
Rade Tontic <r.tontic@vegait.rs>
2023-01-10 12:29:37 +0100

    Remove unused

--- webapp/src/components/Footer/FooterContactSection.tsx
+++ webapp/src/components/Footer/FooterContactSection.tsx
@@ -4,7 +4,6 @@ import { ReactComponent as LinkedinIcon } from "assets/images/linkedin.svg";
 import { ReactComponent as FacebookIcon } from "assets/images/facebook.svg";
 import { ReactComponent as InstagramIcon } from "assets/images/instagram.svg";
 import { IconButton } from "../IconButton/IconButton";
-import { Link } from "../Link/Link";
 
 export const FooterContactSection = () => {
     return (
