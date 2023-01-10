d8f2c38f95be71c9bf3cf4ca7b55c35f1516764c
Ana Svitlica <a.svitlica@vegait.rs>
2023-01-10 16:03:11 +0100

    Show loader while routes are fetched

--- webapp/src/routing/Routes.tsx
+++ webapp/src/routing/Routes.tsx
@@ -4,18 +4,22 @@ import { mapPagesToRoutes } from "./util";
 import { PageReferenceEntry } from "../core/services/contentful/queries/getPageReferences";
 import { useEffect, useState } from "react";
 import { getPageReferences } from "../core/services/contentful/contentful.service";
+import { Loader } from "../components";
 
 export const Routes = () => {
     const [pages, setPages] = useState<PageReferenceEntry[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
 
-    // TODO: Create Loading spinner
     useEffect(() => {
+        setLoading(true);
         getPageReferences()
             .then((res) => {
+                setLoading(false);
                 setPages(res);
             })
             .catch((err) => {
                 // oops
+                setLoading(false);
                 console.log(err);
             });
     }, []);
@@ -24,11 +28,15 @@ export const Routes = () => {
 
     return (
         <BrowserRouter>
-            <BrowserRoutes>
-                <Route path="/" element={<Layout />}>
-                    {routes}
-                </Route>
-            </BrowserRoutes>
+            {loading ? (
+                <Loader center />
+            ) : (
+                <BrowserRoutes>
+                    <Route path="/" element={<Layout />}>
+                        {routes}
+                    </Route>
+                </BrowserRoutes>
+            )}
         </BrowserRouter>
     );
 };
