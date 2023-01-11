8ce645ea067ff151b700d81a1952fa240a803f7f
Kristian Tot <k.tot@vegait.rs>
2023-01-11 14:34:48 +0100

    Remove blogs.json and bring back old service

--- webapp/src/core/services/blog.service.ts
+++ webapp/src/core/services/blog.service.ts
@@ -1,26 +1,24 @@
 import axios from "axios";
 import { Blog } from "../models/blog";
 import { PageOf } from "../models/common/pageOf";
-import * as blogs from "./blogs.json";
 
 export const fetchBlogs = async (pageNumber: number): Promise<PageOf<Blog>> => {
-    // const baseUrl = process.env.REACT_APP_API_URL;
-    // const response = await axios.get<PageOf<Blog>>(`${baseUrl}/api/Blog?pageNumber=${pageNumber}`);
+    const baseUrl = process.env.REACT_APP_API_URL;
+    const { data } = await axios.get<PageOf<Blog>>(`${baseUrl}/api/Blog?pageNumber=${pageNumber}`);
 
-    const data = blogs as unknown as {total: number, items: any[], pageSize: number};
     return new PageOf<Blog>(data.total, data.items, data.pageSize);
 };
 
 export const fetchBlogById = async (id: string): Promise<Blog> => {
     const baseUrl = process.env.REACT_APP_API_URL;
-    const response = await axios.get<Blog>(`${baseUrl}/api/Blog/${id}`);
+    const { data } = await axios.get<Blog>(`${baseUrl}/api/Blog/${id}`);
     return new Blog(
-        response.data.id,
-        response.data.title,
-        response.data.shortDescription,
-        response.data.image,
-        response.data.content,
-        response.data.blogAuthor,
-        response.data.categories
+        data.id,
+        data.title,
+        data.shortDescription,
+        data.image,
+        data.content,
+        data.blogAuthor,
+        data.categories
     );
 };
