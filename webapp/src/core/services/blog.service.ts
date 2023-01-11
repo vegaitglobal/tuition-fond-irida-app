3c83e2ac0015b3f2de8822f08d72a44d8d75b259
Kristian Tot <k.tot@vegait.rs>
2023-01-11 12:04:38 +0100

    Mocked request

--- webapp/src/core/services/blog.service.ts
+++ webapp/src/core/services/blog.service.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { Blog } from "../models/blog";
 import { PageOf } from "../models/common/pageOf";
+import * as blogs from "./blogs.json";
 
 export const fetchBlogs = async (pageNumber: number): Promise<PageOf<Blog>> => {
-    const baseUrl = process.env.REACT_APP_API_URL;
-    const response = await axios.get<PageOf<Blog>>(`${baseUrl}/api/Blog?pageNumber=${pageNumber}`);
-    return new PageOf<Blog>(response.data.total, response.data.items, response.data.pageSize);
+    // const baseUrl = process.env.REACT_APP_API_URL;
+    // const response = await axios.get<PageOf<Blog>>(`${baseUrl}/api/Blog?pageNumber=${pageNumber}`);
+
+    const data = blogs as unknown as {total: number, items: any[], pageSize: number};
+    return new PageOf<Blog>(data.total, data.items, data.pageSize);
 };
 
 export const fetchBlogById = async (id: string): Promise<Blog> => {
